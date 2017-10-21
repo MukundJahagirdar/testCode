2a40093204c68e3cbeb8a9c38cac49218721aba0
DJantikar <deepagjantikar@gmail.com>
2017-10-21 17:28:00 -0400

    Add project files.

---
+++ Thread_static_certification/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread_static_certification
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+        static void Main(string[] args)
+        {
+
+            Thread t = new Thread(() => 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine($" _field value is from t {_field} ");
+                }
+            });
+
+            Thread t1 = new Thread(() =>
+            {
+               
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine($" _field value is  from t1 {_field}");
+                }
+            });
+
+            t.Start();
+             t1.Start();
+
+            t.Join();
+            t1.Join();
+        }
+    }
+}
